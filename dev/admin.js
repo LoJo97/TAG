eeb12bf23b0a8a4baee6c89bc3a0df743005f677
lajordan3 <lajordan3@crimson.ua.edu>
2019-01-07 16:08:48 -0600

    Moved dev apikey to config file and added .gitignore

--- dev/admin.js
+++ dev/admin.js
@@ -1,12 +1,4 @@
 // Initialize Firebase
-var config = {
-	apiKey: "AIzaSyCSYtxFzgNomtvoSBbn3vuN_-kDl61SxyI",
-	authDomain: "watertagtest.firebaseapp.com",
-	databaseURL: "https://watertagtest.firebaseio.com",
-	projectId: "watertagtest",
-	storageBucket: "",
-	messagingSenderId: "781205798625"
-};
 firebase.initializeApp(config);
 
 //Keeps track of data
